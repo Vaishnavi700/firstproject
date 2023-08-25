4595ea24ed0d5396a83c4715a274bcddcf9972a1
[200~Vaishnavi700~ <vaishnavideshmukhr@gmail.com>
2023-08-25 14:06:38 +0530

    first edit

---
+++ 1stwithuser.py
@@ -0,0 +1,17 @@
+
+when =['A few years ago','yesterday','last night','A long time ago','On 20th jan']
+who =['a rabbit','an elephant','a mouse','a turtle','a cat']
+name=['Ali','Miriam','daniel','Hoouk','Starwalker']
+residence=['barcelona','India','Germany','Venice','England']
+went=['cinema','university','seminar','school','laundry']
+happened=['made a lot of friends','Eats a burger','found a secret key','solved a mistery','wrote a book']
+a=int(input('Enter a index value of when:'))
+b=int(input('Enter a index value of who:'))
+c=int(input('Enter a index value of residence:'))
+
+d=int(input('Enter a index value of went:'))
+e=int(input('Enter a index value of happened:'))
+
+print((when[a])+ ', ' +(who[b]) + ' that lived in '+ (residence[c]) + ', went to the ' + (went[d]) + ' and ' +(happened[e]))
+
+
